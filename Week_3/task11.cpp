4f98bde8b602be885fa9a7007e927cebfb44c176
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-25 18:48:23 +0500

    In this C++ program I take 3 student marks from user and find their average.

---
+++ Week_3/task11.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main(){
+  int student1Marks;
+  int student2Marks;
+  int student3Marks;
+  int average;
+  cout<<"Enter student1 marks:";
+  cin>>student1Marks;
+  cout<<"Enter student2 marks:";
+  cin>>student2Marks;
+  cout<<"Enter student3 marks:";
+  cin>>student3Marks;
+  average=(student1Marks+student2Marks+student3Marks)/3;
+  int total;
+  cout<<"enter total marks:";
+  cin>>total;
+  cout<<"Class average is :"<<average;
+} 
