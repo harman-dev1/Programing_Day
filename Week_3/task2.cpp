dcc42f90e247221b621ab3be723d29ee70dbbca7
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-25 18:31:17 +0500

    In this C++ Program I displayed the sum of two integers using C in  and C out statement.

---
+++ Week_3/task2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+int integer1;
+int integer2;
+cout<<" Enter integer1 value:";
+cin>>integer1;
+cout<<"Enter integer2 value:";
+cin>>integer2;
+int sum;
+sum=integer1+integer2;
+cout<<"Sum of integer1 and integer2 is:"<<sum;
+}
