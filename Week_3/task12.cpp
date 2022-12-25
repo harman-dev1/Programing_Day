7939a1be10e6d8de161b53df0759d8731373608f
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-25 18:50:09 +0500

    In this C++ program I calculate user age through his DOB.

---
+++ Week_3/task12.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main(){
+ int date;
+ int month; 
+ int year;
+ cout<<"Enter dd-mm-yyyy:";
+ cin>>date>>month>>year;
+ cout<<date<<"-"<<month<<"-"<<year<<endl;
+ int age;
+ age=2023-year;
+ cout<<"Age is:"<<age;
+}
