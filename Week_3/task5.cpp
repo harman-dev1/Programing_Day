813d2acc8816f027bbcc0bbf4d320be40a380aa8
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-25 18:37:03 +0500

    In this C++ program I displayed how many days require to lose specific weight of user.

---
+++ Week_3/task5.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+main(){
+int weightlose;
+int days=15;
+int requiredDays;
+cout<<"Enter required lose weight:";
+cin>>weightlose;
+requiredDays=weightlose*days;
+cout<<"Required days for losing weight:"<<requiredDays;
+}
