c2bb14f02f95da97cc35564986d4a899557bf055
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2022-12-25 18:47:11 +0500

    In this C++ program I took 2 integers number from user and find sum 5 times.

---
+++ Week_3/task10.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace  std;
+main(){
+int number;
+int sum;
+sum=0;
+cout<<"Enter integer1 :";
+cin>>number;
+sum=number+sum;
+cout<<"Enter integer2:";
+cin>>number;
+sum=sum+number;
+cout<<"Enter integer3:";
+cin>>number;
+sum=sum+number;
+cout<<"Enter integer4:";
+cin>>number;
+sum=sum+number;
+cout<<"Enter integer5:";
+cin>>number;
+sum=sum+number;
+cout<<"Sum of all numbers is:"<<sum;
+}
