682adff0af9ccb6c187abf264712319c8bf44b9c
Harmain Iftikhar <120156793+harman-dev1@users.noreply.github.com>
2023-01-15 21:32:37 +0500

    Add files via upload

---
+++ Week_3/task16.cpp
@@ -0,0 +1,15 @@
+include<iostream>
+using namespace std;
+main()
+{
+    string fruit,day;
+    float quantity;
+    cout<<"Enter fruit ";
+    cin>>fruit;
+    cout<<"Enter day ";
+    cin>>day;
+    cout<<"Enter quantity ";
+    cin>>quantity;
+    if(day == "monday" || day == "tuesday" || day == "thursday" || day == "wednesday" ||day == "f" ||)
+
+}
